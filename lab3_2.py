2991a3884cf34cf20f8cf490a14bcc652947e623
Mykhailo Tokar <mykhailo.tokar@welltech.com>
2022-12-24 22:33:18 +0200

    Update lab3_2.py

--- lab3_2.py
+++ lab3_2.py
@@ -1,8 +1,29 @@
 import os
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+import json
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import mapper, sessionmaker
+from sqlalchemy.ext.declarative import DeclarativeMeta
+
+class AlchemyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            # an SQLAlchemy class
+            fields = {}
+            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                data = obj.__getattribute__(field)
+                try:
+                    json.dumps(data) # this will fail on non-encodable values, like other classes
+                    fields[field] = data
+                except TypeError:
+                    fields[field] = None
+            # a json-encodable dict
+            return fields
+
+        return json.JSONEncoder.default(self, obj)
+
+
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -46,8 +67,10 @@ def new():
         
         session.add(car)
         session.commit()
+    
+    cars = session.query(Car).all()
    
-    return session.query(Car).all()
+    return json.dumps(cars, cls=AlchemyEncoder)
 
 if __name__ == '__main__':
     with app.app_context():
