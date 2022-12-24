3cc76313b013a5014a438d74aefe60914c9fc1df
Mykhailo Tokar <mykhailo.tokar@welltech.com>
2022-12-24 22:08:48 +0200

    another fix

--- lab3_2.py
+++ lab3_2.py
@@ -28,14 +28,15 @@ class Car(db.Model):
 
 @app.route('/cars', methods = ['GET', 'POST'])
 def new():
-   if request.method == 'POST':
-        car = Car(request.form['name'], request.form['tank_capacity'],
-            request.form['petrol_quantity'], request.form['petrol_consumtion_per_100_km'])
-         
-        db.session.add(car)
-        db.session.commit()
+    with app.app_context():
+        if request.method == 'POST':
+            car = Car(request.form['name'], request.form['tank_capacity'],
+                request.form['petrol_quantity'], request.form['petrol_consumtion_per_100_km'])
+            
+            db.session.add(car)
+            db.session.commit()
 
-   return db.session.execute(db.select(Car).order_by(Car.name))
+        return db.session.execute(db.select(Car).order_by(Car.name))
 
 
 if __name__ == '__main__':
